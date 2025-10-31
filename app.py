1fe2612d456059e8ab9b0328053afece0934eb7d
Wenzheng Cai <wenzhengcai@Wenzhengs-Air.fritz.box>
2025-10-31 12:19:51 +0100

    added creating DB tables and user login and register pages

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-from flask import Flask, render_template, request
-from models import db, User, Article, ChatHistory
-from dotenv import load_dotenv
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+from models import db, User, Article, UserArticle, ChatHistory, Statistics
+from modules import news_fetcher, summarizer
 
 import os
+from dotenv import load_dotenv
+from datetime import datetime
 
 # -- Flask Setup --
 load_dotenv()
 
 # Create the App
 app = Flask(__name__)
-
 # Configurate SQLite database
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'data/newsmind.sqlite')}"
@@ -19,13 +20,66 @@ app.secret_key = os.getenv("SECRET_KEY", "dev-secret")
 db.init_app(app)  # Link the database and the app.
 
 
-# -- Route Map
+# -- Route Map ---
 @app.route("/")
-def index():
-    return "Hello World!"
+def home():
+    return render_template("index.html")
 
 
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form.get("username")
+        email = request.form.get("email")
+        password = request.form.get("password")
 
+        if User.query.filter(User.username==username or User.email==email).first():
+            flash("Username or email already exists.")
+            return redirect("/register")
+
+        new_user = User(username=username, email=email)
+        new_user.set_password(password)
+
+        db.session.add(new_user)
+        db.session.commit()
+        flash("Registration successfully, please login.")
+        return redirect("/")
+
+    return render_template("register.html")
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    user = User.query.filter_by(username=request.form.get['username']).first()
+    if user and user.check_password(request.form.get['password']):
+        session['user_id'] = user.id
+        user.last_login = datetime.utcnow()
+        db.session.commit()
+        return redirect('/digest')
+    else:
+        flash("Invalid username or password")
+        return redirect("/")
+
+
+@app.route("/logout")
+def logout():
+    session.pop('user_id', None)
+    flash("Logged out successfully.")
+    return redirect("/")
+
+
+@app.route("/digest")
+def digest():
+    summaries = summarizer.summarize_article()
+    return render_template("digest.html", summaries=summaries)
+
+
+@app.route("/chat", methods=["GET", "POST"])
+def chat():
+    if request.method == "POST":
+        user_input = request.form.get("user_input")
+        # todo: Connect chat_agent here
+    return render_template("chat.html")
 
 
 # --- Run App ---
